fa09bdf3b077fb28e5bee731bdb9944d32994b50
Ryan <ryan.manzer@gmail.com>
2018-06-18 10:43:37 -0700

    Improved packaging and more thorough setup parameters

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
-from setuptools import setup
+import setuptools
 
+with open('README.md','r') as ld:
+  long_description = ld.read()
 
-setup(
+setuptools.setup(
     name = 'todo_catalog',
-    version = '0.1.3',
+    version = '0.1.4',
+    description='A command line application used to generate a TDOD.md file for a given project.'
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/supermanzer/tools',
+    author='C. Ryan Manzer',
+    author_email='ryan.manzer@gmail.com',
     py_modules = ['todo_catalog'],
     install_requires = [
         'Click',
